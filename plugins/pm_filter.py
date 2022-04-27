7a41d3004e71a3b585d228e48b05338fa53276b9
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-04-27 11:14:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -699,7 +699,7 @@ async def auto_filter(client, msg, spoll=False):
              InlineKeyboardButton(text="ɴᴇxᴛ ⏩", callback_data=f"next_{req}_{key}_{offset}")]
         )
 
-        btn.insert(10,
+        btn.insert(0,
             [InlineKeyboardButton(text="🎀 ᴊᴏɪɴ ᴏᴜʀ ɢʀᴏᴜᴘ 🎀",url="https://t.me/movie_lookam")]
         )
 
@@ -707,9 +707,9 @@ async def auto_filter(client, msg, spoll=False):
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
+
+
         btn.insert(0,
-            [InlineKeyboardButton({file.file_name})
-        btn.insert(10,
             [InlineKeyboardButton(text="🎀 ᴊᴏɪɴ ᴏᴜʀ ɢʀᴏᴜᴘ 🎀",url="https://t.me/movie_lookam")]
         )
 
